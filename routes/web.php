7aba2dec4a2d2cd611f9f8991230bb58e053f5c1
heinazhik <heinazhikweymong@outlook.com>
2025-09-30 21:44:58 +0800

    web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 Route::post('/newsletter', NewsletterController::class)->name('newsletter.subscribe');
+Route::get('/healthz', function () {
+    return response()->json(['status' => 'ok']);
+});
