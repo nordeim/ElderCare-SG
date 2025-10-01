24b14f6a8537fa3e61cb5a4572f664169137290b
heinazhik <heinazhikweymong@outlook.com>
2025-10-01 20:38:43 +0800

    apps

--- resources/js/modules/carousel.js
+++ resources/js/modules/carousel.js
@@ -1,13 +1,25 @@
-import EmblaCarousel from 'embla-carousel';
+let emblaModulePromise;
 
 window.initEmbla = (element, options = {}) => {
+    if (!element) {
+        return;
+    }
+
+    if (!emblaModulePromise) {
+        emblaModulePromise = import('embla-carousel').then((module) => module.default);
+    }
+
     const defaultOptions = {
         loop: true,
         align: 'center',
         skipSnaps: false,
     };
 
-    const embla = EmblaCarousel(element, { ...defaultOptions, ...options });
-
-    return embla;
+    emblaModulePromise
+        .then((EmblaCarousel) => {
+            EmblaCarousel(element, { ...defaultOptions, ...options });
+        })
+        .catch((error) => {
+            console.error('Failed to initialize Embla carousel', error);
+        });
 };
